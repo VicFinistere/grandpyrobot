2688d14c9c762a9caac0c85eb4c70e60a68e8102
Zoeinfp <zoebelleton@gmail.com>
2018-08-03 19:04:30 +0200

    Test if input is empty

--- static/js/getting_answer.js
+++ static/js/getting_answer.js
@@ -1,8 +1,57 @@
-$(function () {
-    $('#answer_button').on('click', function () {
-        var input_text = $("#input_text").val();
-//        alert(input_text);
-        $("#answer").append(input_text);
-        });
-    });
+//$(function () {
+//    $('#answer_button').on('click', function () {
+//        var input_text = $("#input_text").val();
+////        alert(input_text);
+//        $("#answer").append(input_text);
+//        });
+//    });
+//
+//$(function ()
+//{
+//    if(characterCode == 13)
+//    {
+//        var key = e.which;
+//        alert("Pushing enter")
+//        var input_text = $("#input_text").val();
+//        if(input_text != "")
+//        {
+//            $("#answer").append(input_text);
+//        }
+//     }
+//});
 
+
+//When pressing a key
+$(document).keypress(function (e) {
+
+    //If the key is enter
+    if (e.which == 13) {
+
+        //Getting the input value
+        var input_text = $("#input_text").val();
+
+        //Getting the content in page
+        var content = $("#answer").html();
+
+        //If the input is filled
+        if(input_text != "")
+        {
+            //We append the input text
+            $("#answer").html(input_text);
+        }
+
+        //If the input if empty
+        else
+        {
+            //It has to be something in input
+            text = "Write something please !..."
+
+            //If it's not already said
+            if(content != text)
+            {
+                //Say it!
+                $("#answer").html(text);
+            }
+        }
+    }
+});
