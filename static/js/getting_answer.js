6ab666a525ac57e3cded90923e8e5f8582f00320
Zoeinfp <zoebelleton@gmail.com>
2018-08-15 13:08:09 +0200

    robot can answer

--- static/js/getting_answer.js
+++ static/js/getting_answer.js
@@ -1,60 +1,33 @@
-//$(function () {
-//    $('#answer_button').on('click', function () {
-//        var input_text = $("#input_text").val();
-////        alert(input_text);
-//        $("#answer").append(input_text);
-//        });
-//    });
-//
-//$(function ()
-//{
-//    if(characterCode == 13)
-//    {
-//        var key = e.which;
-//        alert("Pushing enter")
-//        var input_text = $("#input_text").val();
-//        if(input_text != "")
-//        {
-//            $("#answer").append(input_text);
-//        }
-//     }
-//});
-
-
-//When pressing a key
-$(document).keypress(function (e) {
-
-    //If the key is enter
-    if (e.which == 13) {
-
-        //Getting the input value
-        var input_text = $("#input_text").val();
-
-        //Getting the content in page
-        var content = $("#answer").html();
-
-        //If the input is filled
-        if(input_text != "")
-        {
-            //We append the input text
-            $("#answer").html(input_text);
-
-            //We set the input text value to null
-            $("#input_text").val("");
-        }
-
-        //If the input if empty
-        else
-        {
-            //It has to be something in input
-            text = "Write something please !..."
-
-            //If it's not already said
-            if(content != text)
+function answer()
+{
+    var user_input = $('#text_area').children('p.user_blue_msg').last();
+    var user_text = user_input.html();
+    var user_checked_text = user_text.toLowerCase();
+    switch (true)
+    {
+        case(user_checked_text.indexOf("bonjour") != -1):
+            if (user_input.css('color') != 'rgb(128, 128, 128)')
             {
-                //Say it!
-                $("#answer").html(text);
+                $('<p>', {class: 'robot_white_msg', text: "Bonjour !"}).appendTo('#text_area');
+                user_input.css('color', 'rgb(128,128,128)');
             }
-        }
+            break;
+        case(user_checked_text.indexOf("ok") != -1):
+            if (user_input.css('color') != 'rgb(128, 128, 128)')
+            {
+                $('<p>', {class: 'fas fa-thumbs-up fa-inverse robot_white_msg'}).appendTo('#text_area');
+                user_input.css('color', 'rgb(128,128,128)');
+            }
+            break;
+        default:
+            if (user_input.css('color') != 'rgb(128, 128, 128)')
+            {
+                $('<p>', {class: 'robot_white_msg', text: "..."}).appendTo('#text_area');
+                user_input.css('color', 'rgb(128,128,128)');
+                break;
+            }
     }
-});
+}
+
+setInterval(function(){ answer();  }, 5000);
+//setInterval(function(){ answer();  }, 1000);
