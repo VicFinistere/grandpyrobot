e529978782ad74e4e7eb54374d766b54ec2e8e42
Zoeinfp <zoebelleton@gmail.com>
2018-08-21 12:07:27 +0200

    Cleaning code

--- static/js/sending_text.js
+++ static/js/sending_text.js
@@ -6,8 +6,6 @@ $(function () { $(document).keypress(function (e) { if (e.which === 13){ sending
 
 function sending_text()
 {
-    setInterval(function(){ answer();  }, 1000);
-
     //Getting the input value
     var input_text = $("#input_text").val();
 
@@ -15,23 +13,22 @@ function sending_text()
     var content = $("#answer").html();
 
     //If the input is filled
-    if(input_text != "")
+    if(input_text !== "")
     {
         //We append the input text
         $('<p>', { class: 'user_blue_msg', text: input_text}).appendTo('#text_area');
-
-        //We set the input text value to null
-        $("#input_text").val("");
+        scroll();
+        answer();
     }
 
     //If the input if empty
     else
     {
         //It has to be something in input
-        var text = "Write something please !..."
+        var text = "Write something please !...";
 
         //If it's not already said
-        if(content != text)
+        if(content !== text)
         {
             //Say it!
             $("#answer").html(text);
