adf80387e970900503daac6550cb948176a2c275
Zoeinfp <zoebelleton@gmail.com>
2018-08-21 12:08:05 +0200

    test api file first commit

---
+++ test/test_app.py
@@ -0,0 +1,28 @@
+"""File containing all the unit tests for the application"""
+
+import unittest
+# from unittest.mock import patch
+# import json
+
+# Importer les futurs scripts Python
+app = ""
+
+
+# Examples
+# from bot import logic
+# from bot.views import app as app
+
+class TestFlaskApp(unittest.TestCase):
+    """Unit test class for """
+
+    def setUp(self):
+        app.testing = True
+        # self.app = app.test_client()
+
+
+class TestUtils(unittest.TestCase):
+    """test all functions in the 'utils.py' file"""
+
+
+if __name__ == "__main__":
+    unittest.main()
