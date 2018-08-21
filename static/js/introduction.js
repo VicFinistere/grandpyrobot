ea5a835c0f83bdcbe6b39759c693292c36a95990
Zoeinfp <zoebelleton@gmail.com>
2018-08-21 12:12:12 +0200

    correcting sentence

--- static/js/introduction.js
+++ static/js/introduction.js
@@ -3,7 +3,7 @@ $(function(){ introduction();});
 function introduction()
 {
     $('<p>', {class: 'robot_white_msg', text: "Bonjour ! Je suis grandpy !"}).appendTo('#text_area');
-    $('<p>', {class: 'robot_white_msg', text: "Ecris le nom d'un lieu et je t'en parlerais !"}).appendTo('#text_area');
+    $('<p>', {class: 'robot_white_msg', text: "Ecris le nom d'un lieu et je t'en parlerai !"}).appendTo('#text_area');
     scroll();
 }
 
