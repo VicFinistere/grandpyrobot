71a733a1559d8a046ef0dbe1ff88194cff339352
Zoeinfp <zoebelleton@gmail.com>
2018-08-27 11:13:46 +0200

    No request when the request is empty

--- views.py
+++ views.py
@@ -25,18 +25,22 @@ def calling_google_api():
     """
     input_request = request.args.get('input_text', '')
     cleaned_request = logic.cleaning_request(input_request)
-    place = logic.query_autocomplete_place(cleaned_request)
-    try:
-        maps_response = logic.google_maps_request(place)
-        if maps_response is not None and maps_response is not 'Failed':
-            name, lat, lng, address = maps_response[0], maps_response[1], maps_response[2], maps_response[3]
-            return jsonify(name, lat, lng, address)
-        else:
+    if cleaned_request:
+        place = logic.query_autocomplete_place(cleaned_request)
+        try:
+            maps_response = logic.google_maps_request(place)
+            if maps_response is not None and maps_response is not 'Failed':
+                name, lat, lng, address = maps_response[0], maps_response[1], maps_response[2], maps_response[3]
+                return jsonify(name, lat, lng, address)
+            else:
+                logging.exception("Google maps failed (views.py)")
+                return jsonify('failed')
+
+        except IndexError:
             logging.exception("Google maps failed (views.py)")
             return jsonify('failed')
-
-    except IndexError:
-        logging.exception("Google maps failed (views.py)")
+    else:
+        logging.exception("Google maps failed because of the request emptiness(views.py)")
         return jsonify('failed')
 
 
@@ -48,16 +52,19 @@ def calling_wiki_api():
     """
     user_input = request.args.get('user_input', '')
     cleaned_request = logic.cleaning_request(user_input)
-    data = logic.wiki_request(cleaned_request)
-    if data:
-        logging.info("Wiki wordked with cleaned request (views.py)")
-        return jsonify(data)
-    else:
-        data = logic.wiki_loop_through_keywords(cleaned_request)
+    if cleaned_request:
+        data = logic.wiki_request(cleaned_request)
         if data:
-            logging.info("Wiki wordked with cleagitned request (views.py)")
+            logging.info("Wiki wordked with cleaned request (views.py)")
             return jsonify(data)
         else:
-            logging.exception("Wiki failed (views.py)")
-            return jsonify('failed')
-
+            data = logic.wiki_loop_through_keywords(cleaned_request)
+            if data:
+                logging.info("Wiki wordked with cleagitned request (views.py)")
+                return jsonify(data)
+            else:
+                logging.exception("Wiki failed (views.py)")
+                return jsonify('failed')
+    else:
+        logging.exception("Wiki failed because of the request emptiness(views.py)")
+        return jsonify('failed')
